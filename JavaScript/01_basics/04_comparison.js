469bcc29e8915fdb4665c379fdeb1e82f2c7e537
KraKinMe <dhruvagrawal13021307@gmail.com>
2024-01-13 19:38:34 +0530

    Just a test

--- JavaScript/01_basics/04_comparison.js
+++ JavaScript/01_basics/04_comparison.js
@@ -23,3 +23,27 @@ console.log(null === undefined); // false
 let big = 1234567890123456789012345678901234567890n;
 
 console.log(typeof big); // bigint
+
+//////////////////////////////////////////////////////////////
+
+let a=5
+let b=a
+b=10
+console.log(a) // 5
+console.log(b) // 10
+// a and b are independent
+//but if we have an object
+
+let c={
+    name:"John",
+    age:30
+}
+
+let d=c
+console.log(c) // { name: 'John', age: 30 }
+
+d.age=40
+// Reference to the same object
+console.log(c)
+
+//Here we are not copying the object, but copying the reference to the same object
