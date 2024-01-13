769f065c07a0792bd7010fd19d7ed5ee1f2bccdc
KraKinMe <dhruvagrawal13021307@gmail.com>
2024-01-13 19:27:27 +0530

    f

--- JavaScript/01_basics/04_comparison.js
+++ JavaScript/01_basics/04_comparison.js
@@ -9,3 +9,17 @@ console.log(null >=0); // true (null is converted to 0)
 
 console.log(undefined == 0); // false (undefined is not converted to 0)
 console.log(undefined >= 0); // false (undefined is not converted to 0)
+
+
+//////////////////////////
+//Strict equality operator
+
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false as it checks the type as well
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+let big = 1234567890123456789012345678901234567890n;
+
+console.log(typeof big); // bigint
