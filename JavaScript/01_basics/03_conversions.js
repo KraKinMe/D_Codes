d08c8e6f0e1a66c1030fc3b6be51f8a44c6497ed
KraKinMe <dhruvagrawal13021307@gmail.com>
2024-01-13 19:17:53 +0530

    Basic

---
+++ JavaScript/01_basics/03_conversions.js
@@ -0,0 +1,12 @@
+let score="33abc";
+console.log(typeof (score))
+
+let num=Number(score);
+console.log(typeof (num))
+console.log(num) //NaN (Not a Number)
+
+//****OPERATIONS****//
+
+let value=10;
+let negValue=-value;
+console.log(negValue) //-10
---
+++ JavaScript/01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+console.log(1 == 1); // true
+
+console.log("2">3); // false
+
+console.log(1 == "1"); // true
+
+console.log(null==0) // false (null is not converted to 0)
+console.log(null >=0); // true (null is converted to 0)
+
+console.log(undefined == 0); // false (undefined is not converted to 0)
+console.log(undefined >= 0); // false (undefined is not converted to 0)
