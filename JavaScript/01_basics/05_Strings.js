55a50df3fbcc8cf2d0443f79f95acb1bc0202dc0
KraKinMe <dhruvagrawal13021307@gmail.com>
2024-01-14 13:02:20 +0530

    math

---
+++ JavaScript/01_basics/05_Strings.js
@@ -0,0 +1,12 @@
+const a= "Dhruv"
+let score =500
+
+// console.log(`Hello my name is ${a} scoring ${score}`);
+
+const game= new String('Cricckets')
+console.log(game);
+console.log(typeof game);
+console.log(game[3]);
+console.log(game.toUpperCase());
+console.log(a.toUpperCase());
+console.log(game.indexOf('c'));
---
+++ JavaScript/01_basics/06_nums&maths.js
@@ -0,0 +1,24 @@
+let score=400
+let bonus=new Number(100)
+// console.log(bonus);
+
+// console.log(bonus.toString().length);
+bonus=bonus+0.07
+// console.log(bonus.toFixed(1));
+
+
+/////MATHS/////
+
+
+// console.log(Math.abs(-100));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.round(10.499999));
+
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=1;
+const max=6;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
