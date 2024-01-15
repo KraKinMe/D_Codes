746f1a8da202f2f551da59944ae843ac688002f7
KraKinMe <dhruvagrawal13021307@gmail.com>
2024-01-15 13:08:36 +0530

    h

---
+++ JavaScript/01_basics/07_dates.js
@@ -0,0 +1,14 @@
+let myDate= new Date()
+
+console.log(`
+String:- ${myDate.toString()} 
+DateString:- ${myDate.toDateString()}
+TimeString:- ${myDate.toTimeString()}
+LocaleString:- ${myDate.toLocaleString()}
+LocaleDateString:- ${myDate.toLocaleDateString()}
+ISO:- ${myDate.toISOString()}
+UTC:- ${myDate.toUTCString()}
+:- ${myDate.valueOf()}
+`);
+
+console.log(myDate); 
